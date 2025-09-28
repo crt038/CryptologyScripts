31897c6ab49d3e29d500bb0d54d38bfd42cc6372
Chris Taylor <woogietaylor04@gmail.com>
2025-09-28 19:26:15 -0400

    added decryption with known key

--- scripts/vigenere.py
+++ scripts/vigenere.py
@@ -1,7 +1,7 @@
 import sys
 import util
 
-def encrypt_numbers_vigenere(plaintext_nums: list[int], key_nums: list[int]) -> list[int]:
+def shift_numbers_vigenere(plaintext_nums: list[int], key_nums: list[int]) -> list[int]:
     for i in range (0, len(plaintext_nums)):
         plaintext_nums[i] = (plaintext_nums[i] + (key_nums[i % len(key_nums)])) % 26
     return plaintext_nums
@@ -13,7 +13,7 @@ def encrypt(plaintext: str, key:str) -> str:
     except ValueError as e:
         print(e)
         sys.exit(1)
-    shifted_plaintext_numbers = encrypt_numbers_vigenere(plaintext_numbers, 
+    shifted_plaintext_numbers = shift_numbers_vigenere(plaintext_numbers, 
                                                          key_numbers)
     try:
         return util.convert_number_list_to_string(shifted_plaintext_numbers)
@@ -22,10 +22,24 @@ def encrypt(plaintext: str, key:str) -> str:
         sys.exit(1)
 
 def decrypt_with_known_key(ciphertext: str, key:str) -> str:
-    return 'DECRYPTION NOT IMPLEMENTED YET'
+    try:
+        ciphertext_numbers = util.convert_string_to_number_list(ciphertext)
+        key_numbers = util.convert_string_to_number_list(key)
+    except ValueError as e:
+        print(e)
+        sys.exit(1)
+    for i in range (0, len(key_numbers)):
+        key_numbers[i] = (26 - key_numbers[i]) % 26
+    shifted_ciphertext_numbers = shift_numbers_vigenere(ciphertext_numbers,
+                                                          key_numbers)
+    try:
+        return util.convert_number_list_to_string(shifted_ciphertext_numbers)
+    except ValueError as e:
+        print(e)
+        sys.exit(1)
 
 def main():
-    print('CLI tools are not ready yet!')
+    print('CLI TOOLS ARE NOT READY YET!')
 
 if __name__ == '__main__':
     main()
