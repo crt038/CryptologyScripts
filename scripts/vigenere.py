be6815271c0906c07bc71fac8d915b6566b62b29
Chris Taylor <woogietaylor04@gmail.com>
2025-09-28 19:04:30 -0400

    adding encryption for vigenere cipher

---
+++ scripts/vigenere.py
@@ -0,0 +1,31 @@
+import sys
+import util
+
+def encrypt_numbers_vigenere(plaintext_nums: list[int], key_nums: list[int]) -> list[int]:
+    for i in range (0, len(plaintext_nums)):
+        plaintext_nums[i] = (plaintext_nums[i] + (key_nums[i % len(key_nums)])) % 26
+    return plaintext_nums
+
+def encrypt(plaintext: str, key:str) -> str:
+    try:
+        plaintext_numbers = util.convert_string_to_number_list(plaintext)
+        key_numbers = util.convert_string_to_number_list(key)
+    except ValueError as e:
+        print(e)
+        sys.exit(1)
+    shifted_plaintext_numbers = encrypt_numbers_vigenere(plaintext_numbers, 
+                                                         key_numbers)
+    try:
+        return util.convert_number_list_to_string(shifted_plaintext_numbers)
+    except ValueError as e:
+        print(e)
+        sys.exit(1)
+
+def decrypt_with_known_key(ciphertext: str, key:str) -> str:
+    return 'DECRYPTION NOT IMPLEMENTED YET'
+
+def main():
+    print('CLI tools are not ready yet!')
+
+if __name__ == '__main__':
+    main()
